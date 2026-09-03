3d4bfe3b1f3fee57a5860781b5784f35a041ddb3
Stockmite <rojogreenazul2@gmail.com>
2026-09-03 16:28:41 -0300

    Added direction to (almost) all the measurements used up until now

--- baseclasses.cpp
+++ baseclasses.cpp
@@ -4,17 +4,20 @@ struct Coords {
 
     double X;
     double Y;
+    double Z;
 
 }; using Coordinates = struct Coords;
+using Vector2D = Coordinates;
 
 class Point {
 
     //TODO: Alter the code such that "point" inherits from a future class called "Matter"
     Coordinates position;
     double Mass;
-    double Velocity;
-    double Acceleration;
-    double Momentum;
+    Vector2D Velocity;
+    Vector2D Acceleration;
+    Vector2D Momentum;
+    Vector2D NetForce;
 
 };
 
